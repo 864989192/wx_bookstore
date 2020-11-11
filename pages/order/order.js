b497a7253a92d8c423ab4b7224f75970a42ffa57
y864989192 <864989192@qq.com>
2020-11-11 17:36:09 +0800

    ranking
    ranking

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,55 +1,29 @@
-// pages/ranking/ranking.js
+// pages/order/order.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // 控制nav的导航
-    classindex: '0',
-    // 控制页面导航
-    rnav: true,
-    // 控制编辑全选
-    select: true,
-    // redio选择
-    allchecked:"",
-    // 选择的值
-    allcheckvalues:[]
+classindex:'0',
+// 畅销榜
+csb:['畅销榜','畅销榜','畅销榜'],
   },
 // 导航点击事件
-  changeindex(e) {
+changeindex(e) {
+  this.setData({
+    classindex: e.target.dataset.index
+  })
+  if (e.target.dataset.index == '1') {
     this.setData({
-      classindex: e.target.dataset.index
+      rnav: false
     })
-    if (e.target.dataset.index == '1') {
-      this.setData({
-        rnav: false
-      })
-    } else {
-      this.setData({
-        rnav: true
-      })
-    }
-  },
-  // 编辑点击事件
-  change(e) {
-    if (e.target.dataset.select == "0") {
-      this.setData({
-        select: false
-      })
-    } else {
-      this.setData({
-        select: true
-      })
-    }
-  },
-  // 全部选择
-  allselect(){
-    this.setData({allchecked:true})
-  },
-  allcheckvalue(e){
-    console.log(e.detail.value)
-  },
+  } else {
+    this.setData({
+      rnav: true
+    })
+  }
+},
   /**
    * 生命周期函数--监听页面加载
    */
